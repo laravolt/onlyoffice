183fa391519a02ef3a680a46a08bb0c1284965ac
akbar <mochakbar.2124@students.amikom.ac.id>
2022-01-14 14:59:46 +0700

    add menu onlyoffice

---
+++ config/onlyoffice.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'menu' => [
+        'enabled' => true,
+        'label' => 'only',
+    ],
+    'collections' => [
+        'template' => [
+            'label' => 'Templates',
+        ],
+    ],
+];
--- src/OnlyOfficeServiceProvider.php
+++ src/OnlyOfficeServiceProvider.php
@@ -24,9 +24,29 @@ class OnlyOfficeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->menu();
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'onlyoffice');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations/2022_01_10_021527_create_onlyoffice_tokens_table.php');
         Blade::component('onlyoffice', OnlyOfficeComponent::class);
+    }
+
+    protected function menu()
+    {
+        app('laravolt.menu.sidebar')->register(function ($menu) {
+            $menu = $menu->system;
+            $group = $menu->add(__('OnlyOffice'))
+                ->data('icon', 'list')
+                ->data('order', 1);
+                // ->data('permission', config('laravolt.lookup.permission'));
+
+            foreach (config('laravolt.lookup.collections') as $key => $collection) {
+                $menu = $group->add($collection['label'], url("lookup/{$key}"))
+                    ->active('lookup/'.$key.'/*');
+                foreach ($collection['data'] ?? [] as $dataKey => $dataValue) {
+                    $menu->data($dataKey, $dataValue);
+                }
+            }
+        });
     }
 }
