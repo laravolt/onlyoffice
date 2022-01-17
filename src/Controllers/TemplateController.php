c4073d1d54e8e4aac1b3d638a635439d7e46a8cd
akbar <mochakbar.2124@students.amikom.ac.id>
2022-01-17 11:42:55 +0700

    add function upload file

--- src/Controllers/TemplateController.php
+++ src/Controllers/TemplateController.php
@@ -4,6 +4,7 @@ namespace Laravolt\OnlyOffice\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Http;
 use Laravolt\OnlyOffice\Models\OnlyOfficeTokens;
 
 class TemplateController extends Controller
@@ -46,7 +47,14 @@ class TemplateController extends Controller
      */
     public function store(Request $request, $id)
     {
-        dd($request->all(), $id);
+        $res = Http::withHeaders(["Authorization" => $this->token])
+                    ->attach('file', file_get_contents($request->file), $request->file->getClientOriginalName())
+                    ->post(config()->get('services.onlyoffice.groupoffice_url')."/api/2.0/files/$id/upload");
+        if ($res->successful()) {
+            return redirect()->to(route('onlyoffice::template.index', $id))->withSuccess('Berhasil upload templates');
+        } else {
+            return redirect()->back()->withErrors('Gagal upload templates');
+        }
     }
 
     /**
